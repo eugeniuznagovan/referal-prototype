e2d4889388ef22670caffa360ff36091d3653eee
ng-babyshark <eugeniuznagovan@gmail.com>
2025-01-02 21:27:28 +0200

    Added ion-page class to home page

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -11,40 +11,54 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
-  IonNavLink
+  IonNavLink,
 } from '@ionic/angular/standalone';
 import { ContactsComponent } from './contacts.component';
 
 @Component({
   selector: 'app-home',
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardHeader, IonCardContent, IonCardSubtitle, IonButton, RouterLink],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardTitle,
+    IonCardHeader,
+    IonCardContent,
+    IonCardSubtitle,
+    IonButton,
+    RouterLink,
+  ],
   template: `
-    <ion-header>
-      <ion-toolbar color="primary">
-        <ion-title>Home</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content>
-      <ion-card>
-        <img
-        width="355"
-        height="178"
-          alt="Silhouette of mountains"
-          src="https://ionicframework.com/docs/img/demos/card-media.png"
-        />
-        <ion-card-header>
-          <ion-card-title>Contacts</ion-card-title>
-          <ion-card-subtitle>Invite your friends!</ion-card-subtitle>
-        </ion-card-header>
+    <div class="ion-page">
+      <ion-header>
+        <ion-toolbar color="primary">
+          <ion-title>Home</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <ion-card>
+          <img
+            width="355"
+            height="178"
+            alt="Silhouette of mountains"
+            src="https://ionicframework.com/docs/img/demos/card-media.png"
+          />
+          <ion-card-header>
+            <ion-card-title>Contacts</ion-card-title>
+            <ion-card-subtitle>Invite your friends!</ion-card-subtitle>
+          </ion-card-header>
 
-        <ion-card-content>
-          <ion-button routerLink="contacts">Navigate</ion-button>
-        </ion-card-content>
-      </ion-card>
-    </ion-content>
+          <ion-card-content>
+            <ion-button routerLink="contacts">Navigate</ion-button>
+          </ion-card-content>
+        </ion-card>
+      </ion-content>
+    </div>
   `,
   styles: ``,
 })
 export class HomeComponent {
-  component = ContactsComponent
+  component = ContactsComponent;
 }
