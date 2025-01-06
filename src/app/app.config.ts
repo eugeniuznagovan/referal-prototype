87264d54e4d8ad28918b82501bcb381f7d6c72ce
ng-babyshark <eugeniuznagovan@gmail.com>
2025-01-06 22:36:57 +0200

    Request full screen

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,26 @@
-import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+  InjectionToken,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { WebApp} from '@twa-dev/types';
+
+export const TELEGRAM_WEB_APP = new InjectionToken<WebApp>('WindowToken', {
+  providedIn: 'root',
+  //@ts-ignore TODO: Remove ts-ignore
+  factory: () => window.Telegram.WebApp,
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
-    provideIonicAngular({mode: 'ios'}),
+    provideIonicAngular({ mode: 'ios' }),
   ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { appConfig, TELEGRAM_WEB_APP } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import WebApp from '@twa-dev/sdk';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(appRef => {
+    WebApp.ready();
+    console.log(WebApp.version);
+
+    
+    const injector = appRef.injector;
+    const telegramWebApp = injector.get(TELEGRAM_WEB_APP);
+    telegramWebApp.requestFullscreen();
+  })
   .catch((err) => console.error(err));
