b9db76f41e9ac3e77559504769bc77999a7f6b30
ng-babyshark <eugeniuznagovan@gmail.com>
2025-01-30 23:44:39 +0200

    Added telegram user to app.store.ts

--- src/app/store/app.store.ts
+++ src/app/store/app.store.ts
@@ -1,19 +1,44 @@
 import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
-import { computed, inject } from '@angular/core';
-import { TwaService } from '../services/twa.service';
+import { computed, inject, isDevMode } from '@angular/core';
+import { WebAppUser } from '@twa-dev/types';
+import { TWA_TOKEN } from '../app.config';
 
 export type Theme = 'light' | 'dark';
 
-export interface App {
-  theme: Theme
+export interface AppState {
+  theme: Theme,
+  user: WebAppUser;
 }
 
-export const initialState: App = {
-  theme: 'light',
-};
-
 export const AppStore = signalStore(
-  withState(initialState),
+  withState((twaService = inject(TWA_TOKEN)): AppState => {
+    if (isDevMode()) {
+      return {
+        theme: 'light',
+        user: {
+          id: 123456789,
+          is_bot: false,
+          first_name: "John",
+          last_name: "Doe",
+          username: "johndoe",
+          language_code: "en",
+          is_premium: true,
+          photo_url: "https://example.com/photo.jpg",
+        },
+      }
+    }
+
+    const user = twaService?.initDataUnsafe?.user;
+
+    if (!user) {
+      throw new Error('The app can work only as a telegram mini app');
+    }
+
+    return {
+      theme: twaService.colorScheme,
+      user: user
+    }
+  }),
   withComputed(({theme}) => ({
     isLightTheme: computed(() => theme() === 'light')
   })),
@@ -24,8 +49,8 @@ export const AppStore = signalStore(
     }
   })),
   withHooks({
-    onInit(store, twaService = inject(TwaService)) {
-      store.setTheme(twaService.twa.colorScheme);
+    onInit(store) {
+      store.setTheme(store.theme());
     }
   })
 )
